0cea1fdbb2701192f65829595ccdd4d3b4e7d965
zerobits01 <zerobits01@yahoo.com>
2019-12-10 20:34:24 +0330

    changed adder-tree with for-gen

--- conv2/adder_tree.vhd
+++ conv2/adder_tree.vhd
@@ -30,36 +30,111 @@ architecture Behavioral5 of adder_tree5 is
 	signal t1,t2,t3,t4,t5,t6 : int_arr;
 	signal counter : integer := 0;
 begin
-	gen : for i in 0 to 4 generate
-		inner_gen : for j in 0 to 4 generate
-			t1(counter) <= in_1(i)(j);
-			counter <= counter+1;
-		end generate;
-	end generate;
-	al_25_13 : adder_levels Generic Map(N => 24) Port Map(
-		out_1 => t2,
-		in_1 => t1
-	);
-	
-	al_13_7 : adder_levels Generic Map(N => 12) Port Map(
-		out_1 => t3,
-		in_1 => t2
-	);
-	
-	al_7_4 : adder_levels Generic Map(N => 6) Port Map(
-		out_1 => t4,
-		in_1 => t3
-	);
-	
-	al_4_2 : adder_levels Generic Map(N => 3) Port Map(
-		out_1 => t5,
-		in_1 => t4
-	);
+	gen0 : for i in 0 to 6 generate
+		
+		inner0:if i = 0 generate 
+			gen : for i in 0 to 4 generate
+				inner_gen : for j in 0 to 4 generate
+					t1(counter) <= in_1(i)(j);
+					counter <= counter+1;
+				end generate;
+			end generate;
+		end generate inner0;
+		
+		inner1:if i = 1 generate
+			al_25_13 : adder_levels Generic Map(N => 24) Port Map(
+				out_1 => t2,
+				in_1 => t1
+			);
+		end generate inner1;
+		
+		inner2:if i = 2 generate
+			al_13_7 : adder_levels Generic Map(N => 12) Port Map(
+				out_1 => t3,
+				in_1 => t2
+			);
+		end generate inner2;
+		
+		inner3:if i = 3 generate
+			al_7_4 : adder_levels Generic Map(N => 6) Port Map(
+				out_1 => t4,
+				in_1 => t3
+			);
+		end generate inner3;
 
-	al_2_1 : adder_levels Generic Map(N => 1) Port Map(
-		out_1 => t6,
-		in_1 => t5
-	);
-	out_1 <= t6(0);
+		inner4:if i = 4 generate
+			al_4_2 : adder_levels Generic Map(N => 3) Port Map(
+				out_1 => t5,
+				in_1 => t4
+			);
+		end generate inner4;
+
+		inner5:if i = 5 generate
+			al_2_1 : adder_levels Generic Map(N => 1) Port Map(
+				out_1 => t6,
+				in_1 => t5
+			);
+		end generate inner5;
+
+		inner6:if i = 6 generate
+			out_1 <= t6(0);
+		end generate inner6;
+
+	end generate gen0;
 end Behavioral5;
 
+--	process(in_1)
+--	begin
+--		gen : for i in 0 to 4 loop
+--			inner_gen : for j in 0 to 4 loop
+--				t1(counter) <= in_1(i)(j);
+--				counter <= counter+1;
+--			end loop;
+--		end loop;
+--	end process;
+--	
+--	process(t2)
+--	begin
+--		al_25_13 : adder_levels Generic Map(N => 24) Port Map(
+--			out_1 => t2,
+--			in_1 => t1
+--		);
+--	end process;
+--	
+--	process(t2)
+--	begin
+--		al_13_7 : adder_levels Generic Map(N => 12) Port Map(
+--			out_1 => t3,
+--			in_1 => t2
+--		);
+--	end process;
+--
+--	
+--	process(t3)
+--	begin
+--		al_7_4 : adder_levels Generic Map(N => 6) Port Map(
+--			out_1 => t4,
+--			in_1 => t3
+--		);
+--	end process;
+--	
+--	process
+--	begin
+--		al_4_2 : adder_levels Generic Map(N => 3) Port Map(
+--			out_1 => t5,
+--			in_1 => t4
+--		);
+--	end process;
+--
+--	process(t5)
+--	begin
+--		al_2_1 : adder_levels Generic Map(N => 1) Port Map(
+--			out_1 => t6,
+--			in_1 => t5
+--		);
+--	end process;
+--
+--	process(t5)
+--	begin
+--		out_1 <= t6(0);
+--	end process;
