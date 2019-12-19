e8e565287d8bce6023be4b0e890469f6fae5d151
zerobits01 <zerobits01@yahoo.com>
2019-12-19 20:52:22 +0330

    faze2 done!

--- conv2/PAM_TOP.vhd
+++ conv2/PAM_TOP.vhd
@@ -5,14 +5,36 @@ use work.arr_mat.all;
 
 entity PAM_TOP is
 	Port(
-		
+			in_1 : in int_mat;
+			in_2 : in int_mat;
+			clk  : in Bit; 
+			out_1: out integer
 	);
 end PAM_TOP;
 
 architecture Behavioral of PAM_TOP is
-
+	component PAM5 is
+		Generic(N : integer := 4);
+		Port(
+			in_1 : in int_arr;
+			in_2 : in int_arr;
+			clk  : in Bit;
+			out_1: out integer;
+			temp : OUT integer
+		);
+	end component PAM5;
+	signal temp : int_arr := (others => 0);
 begin
-
-
+	gen : for i in 0 to 5 generate
+		pam : PAM5 port map(
+			in_1 => in_1(i),
+			in_2 => in_2(i),
+			clk => clk,
+			out_1 => temp(i)
+		); 
+		gen1 : if i = 5 generate
+			out_1 <= temp(0) + temp(1) + temp(2) + temp(3) + temp(4);
+		end generate gen1;
+	end generate;
 end Behavioral;
 
---
+++ conv2/PAM_TOP_TB.vhd
@@ -0,0 +1,75 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+library work;
+use work.arr_mat.all;
+ 
+ENTITY PAM_TOP_TB IS
+END PAM_TOP_TB;
+ 
+ARCHITECTURE behavior OF PAM_TOP_TB IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT PAM_TOP
+    PORT(
+         in_1 : IN  int_mat;
+         in_2 : IN  int_mat;
+         clk : IN  Bit;
+         out_1 : OUT  integer
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal in_1 : int_mat := (
+				(1,1,1,others => 0),
+				(2,2,3,others => 0),
+				others => (others => 0)
+			);
+   signal in_2 : int_mat:= (
+				(1,1,1,others => 0),
+				(2,2,2,others => 0),
+				others => (others => 0)
+			);
+   signal clk : Bit := '0';
+
+ 	--Outputs
+   signal out_1 : integer;
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: PAM_TOP PORT MAP (
+          in_1 => in_1,
+          in_2 => in_2,
+          clk => clk,
+          out_1 => out_1
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for clk_period*10;
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
